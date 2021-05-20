d90e3b04b235859e32f3fff631561ba5c3d69ede
Ignacio Arriagada <iarriagada@gemini.edu>
2021-05-20 11:51:59 -0400

    Optimized website filtering part of the code

--- gem-mask-fix.py
+++ gem-mask-fix.py
@@ -7,7 +7,7 @@ from datetime import datetime
 
 ictd_url = "http://sbfictd01.cl.gemini.edu/ChangeRequestGMOScassette.php"
 # files_dir = "/net/export/icarus/gemini/GEM8.6/gmos/gmos/data/"
-files_dir = "./"
+files_dir = "./test_files/"
 msk_file = "msk.lut"
 masks_file = "masks.lut"
 msk_path = files_dir + msk_file
@@ -21,15 +21,22 @@ len_n_max = len_nb - len_bar + 1 # smfh
 class NameTooLongError(ValueError):
     pass
 
+# Custom error for when the name of the barcode is too long for the file (smh)
+class ICTDPageError(ValueError):
+    pass
+
 # Get the content from the ICTD site
-ictd_resp = urllib.request.urlopen(ictd_url)
-ictd_content = [l.decode('utf-8').strip('\n')
-                for l in ictd_resp.readlines()
-                if l.decode('utf-8').strip('\n')]
+try:
+    ictd_resp = urllib.request.urlopen(ictd_url)
+    ictd_content = [l.decode('utf-8').strip('\n')
+                    for l in ictd_resp.readlines()
+                    if l.decode('utf-8').strip('\n')]
+except URLError as e:
+    print(e)
+    sys.exit(f"There was a problem accessing {ictd_url}")
 
 cassette_tables = {} # Dictionary for cassette data
 # Flags for site content filtering
-table_flag = False
 cassette_flag = False
 tr_flag = False
 # Column number
@@ -37,47 +44,45 @@ i = 1
 
 # Filter the contents of the ICTD webpage
 for l in ictd_content:
-    # Check for the beginning of the cassette section
-    l_filt = re.search(r'OUTPUT CASSETTES', l)
+    # Check for the begginning of a cassette table
+    l_filt = re.search(r'Cassette ([123])', l)
+    # Check if a cassette table has already been found
+    if l_filt and cassette_flag:
+        cassette_flag = False
     if not(l_filt) and not(cassette_flag):
         continue
+    # Cassette table found
     if not(cassette_flag):
+        cassette_no = l_filt.group(1)
+        # Initialize cassette dictionary
+        cassette_tables['c'+cassette_no] = {}
         cassette_flag = True
-    # Check for the begginning of a cassette table
-    l_filt = re.search(r'[^/]TABLE', l)
-    if not(l_filt) and not(table_flag):
         continue
-    if not(table_flag):
-        # cassette_tables['c'+str(i)] = []
-        cassette_tables['c'+str(i)] = {}
-        table_flag = True
-    # Check for the end of a cassette table
-    l_filt = re.search(r'/TABLE', l)
-    if l_filt and table_flag:
-        table_flag = False
-        i += 1
-        continue
-    # Check for the slot number
-    l_filt = re.search(r'>([0-9])<',l)
+    # Check for the end of the cassette tables section and stop search
+    l_filt = re.search(r'END OUTPUT',l)
+    if l_filt:
+        break
+    # Find a slot with a mask
+    l_filt = re.search(r'>[0-9]<',l)
     if not(l_filt) and not(tr_flag):
         continue
     if not(tr_flag):
-        # aux = [l_filt.group(1)]
         tr_flag = True
         continue
     # Check for the mask name and serial number
     l_filt = re.findall(r'<TD>([^<]+)</TD>',l)
     if l_filt and tr_flag:
-        # Add dict entry name and serial
-        cassette_tables['c'+str(i)][l_filt[1]] = l_filt[0]
+        # Add barcode dict entry for mask name
+        cassette_tables['c'+cassette_no][l_filt[1]] = l_filt[0]
         tr_flag = False
-    # Check for end of Cassette section and stop checking webpage
-    l_filt = re.search(r'END OUTPUT', l)
-    if l_filt:
-        break
+
+if not(cassette_tables):
+    raise ICTDPageError(
+        'No data was obtained from ICTD page. Webpage format may have changed'
+    )
 
 cassette_tables['c1']['42069666'] = 'The beast'
-# cassette_tables['c3']['99999999'] = 'Que wa te paso a ti'
+cassette_tables['c3']['99999999'] = 'Que wa te paso a ti'
 
 # Read msk.lut and generate a list with installed masks
 try:
@@ -107,9 +112,9 @@ if not(unknown_masks):
 file_lines = []
 for m in unknown_masks:
     # Most of this code is to format the entry into masks.lut (smh)
-    col = 'c' + msk_dict[m]
+    cn = 'c' + msk_dict[m]
     try:
-        name = cassette_tables[col][m]
+        name = cassette_tables[cn][m]
     except KeyError:
         print(f"Error: No information available for {m} barcode in ICTD")
     else:
